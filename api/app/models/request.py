86f849f3a701d868d652600f6b4f1900732b7379
blac-siren <zac.blacksiren@gmail.com>
2018-05-30 12:03:10 +0300

    [Feature #157927630] Add request model

--- api/app/models/request.py
+++ api/app/models/request.py
@@ -1,1 +1,22 @@
+"""Module for request."""
 
+
+class Request:
+    """Class saves request."""
+
+    requests = []
+
+    def __init__(self, request, category, location):
+        self.request = request
+        self.category = category
+        self.location = location
+
+    def save_request(self):
+        """Save request in a dictionary."""
+        request = {
+            'request': self.request,
+            'category': self.category,
+            'location': self.location
+        }
+        Request.requests.append(request)
+        return request
