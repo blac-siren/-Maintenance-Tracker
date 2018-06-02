bcedff2452bf66ee9e37fb7c711fbb56bce074f0
blac-siren <zac.blacksiren@gmail.com>
2018-06-02 13:52:40 +0300

    [Chore] Add generate token

--- api/app/models/user.py
+++ api/app/models/user.py
@@ -2,7 +2,7 @@
 from flask_bcrypt import Bcrypt
 import datetime
 import uuid
-import jwt
+# import jwt
 
 # local imports
 from app import app
@@ -11,7 +11,7 @@ from app import app
 class User:
     """Store user info into dictionary."""
 
-    user_info = {}
+    user_info = []
 
     def __init__(self, username, email, password):
         self.public_id = str(uuid.uuid4())
@@ -23,25 +23,26 @@ class User:
     def create_user(self):
         """Save user info into dictionary."""
         user_detail = {
+            'id': len(User.user_info) + 1,
             "email": self.email,
             'username': self.username,
-            'id': self.public_id,
+            'user_id': self.public_id,
             'password': self.password_hash
         }
 
-        User.user_info.update(user_detail)
+        User.user_info.append(user_detail)
         return user_detail
 
-    def generate_token(self, user_id):
-        """Generate Auth Token."""
-        payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
-            'iat': datetime.datetime.utcnow(),
-            'sub': user_id
-        }
-        return jwt.encode(
-            self, payload, app.config.get('SECRET_KEY'), algorithm='HS256')
+    # def generate_token(self, user_id):
+    #     """Generate Auth Token."""
+    #     payload = {
+    #         'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+    #         'iat': datetime.datetime.utcnow(),
+    #         'sub': user_id
+    #     }
+    #     return jwt.encode(
+    #         self, payload, app.config.get('SECRET_KEY'), algorithm='HS256')
 
-    def __repr__(self):
-        """Representation of the model."""
-        return '<Username> {}'.format(self.username)
+    # def __repr__(self):
+    #     """Representation of the model."""
+    #     return '<Username> {}'.format(self.username)
