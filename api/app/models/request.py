114c1ce768db155d84bee25fb523515999f59ab6
blac-siren <zac.blacksiren@gmail.com>
2018-06-03 11:58:15 +0300

    [Feature #157927630] Refactor code

--- api/app/models/request.py
+++ api/app/models/request.py
@@ -6,20 +6,19 @@ class CreateRequest:
 
     all_requests = []
 
-    def __init__(self, request, category, location):
+    def __init__(self, user_request, category, location):
 
-        self.req = req
+        self.user_request = user_request
         self.category = category
         self.location = location
 
     def save_request(self):
         """Save request in a dictionary."""
         requests = {
-                'id': len(CreateRequest.all_requests) +1
-                'request': self.request,
-                'category': self.category,
-                'location': self.location
-            }
+            'id': len(CreateRequest.all_requests) + 1,
+            'user_request': self.user_request,
+            'category': self.category,
+            'location': self.location
         }
 
         CreateRequest.all_requests.append(requests)
