92b3799052982e18c4dbe30a4de6a5c4dc0378a9
blac-siren <zac.blacksiren@gmail.com>
2018-06-03 18:19:31 +0300

    [Chore] Update tests

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -15,20 +15,19 @@ class UserApiTestCase(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client
         self.user_details = {
-            'username': 'Test',
-            'email': 'example@example.com',
-            'password': 'test_password',
+            'username': 'Joe_test',
+            'email': 'joe@email.com',
+            'password': 'U#76pJr3r',
         }
 
     def tearDown(self):
         """Tear down all initialized variables"""
-
-        User.user_info.clear()
+        del User.user_info[:]
 
     def test_registration(self):
         """Tests if user registration works correctly"""
         res = self.client().post(
-            'api/v1/register',
+            'api/v1/auth/register',
             content_type="application/json",
             data=json.dumps(self.user_details))
         # get the results in json format
@@ -37,13 +36,12 @@ class UserApiTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 201)
 
     def test_registration_with_short_password_provided(self):
-        """Makes a post request to the api with a password and tests if user
-        will be registered"""
+        """Test api if user register with short password"""
         res = self.client().post(
-            'api/v1/register',
+            'api/v1/auth/register',
             data=json.dumps({
-                'username': 'Test',
-                'email': 'example@example.com',
+                'username': 'Joe',
+                'email': 'Joe@example.com',
                 'password': 'test'
             }),
             content_type="application/json")
@@ -52,54 +50,126 @@ class UserApiTestCase(unittest.TestCase):
         result = json.loads(res.data)
         self.assertEqual(result['Message'], 'Password must be greater than 8')
 
-    def test_login_password_mismatch(self):
-        """Makes a post request to the api with wrong password and test login"""
+    def test_registration_with_less_data_provided(self):
+        """Test api when user provide less input data required for registration"""
         res = self.client().post(
-            'api/v1/register',
-            content_type="application/json",
-            data=json.dumps(self.user_details))
-
-        login_res = self.client().post(
-            'api/v1/login',
+            'api/v1/auth/register',
             data=json.dumps({
-                'email': 'example@example.com',
-                'password': "wrongpass"
+                'username': 'Joe',
+                'email': 'Joe@example.com',
             }),
             content_type="application/json")
-        res = json.loads(login_res.data)
-        self.assertEqual(res['Message'], 'Incorrect Email or Password ')
+
+        # get the result in json format
+        result = json.loads(res.data)
+        self.assertEqual(result['Message'], 'All input data required!')
+        self.assertEqual(res.status_code, 400)
+
+    def test_registration_with_invalid_email_syntax(self):
+        """Test api when user register with invalid email syntax"""
+        res = self.client().post(
+            'api/v1/auth/register',
+            data=json.dumps({
+                'username': 'Joe',
+                'email': 'apple.com',
+                'password': 'U#76pJr3r'
+            }),
+            content_type="application/json")
+
+        # get the result in json format
+        result = json.loads(res.data)
+        self.assertEqual(result['Message'],
+                         'apple.com is not a valid email address')
+        self.assertEqual(res.status_code, 400)
+
+    def test_registration_with_short_email(self):
+        """Test api when user register with invalid email syntax"""
+        res = self.client().post(
+            'api/v1/auth/register',
+            data=json.dumps({
+                'username': 'Joe',
+                'email': 'a@a',
+                'password': 'U#76pJr3r'
+            }),
+            content_type="application/json")
+
+        # get the result in json format
+        result = json.loads(res.data)
+        self.assertEqual(result['Message'],
+                         'Email address must be 6 characters or more')
+        self.assertEqual(res.status_code, 411)
 
     def test_user_login(self):
         """Test user login"""
         self.client().post(
-            'api/v1/register',
+            'api/v1/auth/register',
             data=json.dumps(self.user_details),
             content_type="application/json")
 
         login_res = self.client().post(
-            'api/v1/login',
+            'api/v1/auth/login',
             data=json.dumps(self.user_details),
             content_type="application/json")
 
         results = json.loads(login_res.data)
         self.assertEqual(results['Message'], 'Successfully logged in')
 
-    # def test_unregistered_user_login(self):
-    #     none_exist = {"email": "asgh@googligoo.com", "password": 'bbbbasdghj'}
-    #     login_res = self.client().post('api/v1/auth/login', data=none_exist)
-    #     result = json.loads(login_res)
-    #     self.assertEqual(result['message'], 'Incorrect Email or Password')
+    def test_login_with_lesss_input_data(self):
+        """Test user login"""
+        self.client().post(
+            'api/v1/auth/register',
+            data=json.dumps(self.user_details),
+            content_type="application/json")
+
+        login_res = self.client().post(
+            'api/v1/auth/login',
+            data=json.dumps({
+                "email": "joedoe@email.com"
+            }),
+            content_type="application/json")
+
+        results = json.loads(login_res.data)
+        self.assertEqual(results['Message'],
+                         'Invalid, all input data required!')
+        self.assertEqual(login_res.status_code, 400)
+
+    def test_login_password_mismatch(self):
+        """Makes a post request to the api with wrong password and test login"""
+        res = self.client().post(
+            'api/v1/auth/register',
+            content_type="application/json",
+            data=json.dumps(self.user_details))
+
+        login_res = self.client().post(
+            'api/v1/auth/login',
+            data=json.dumps({
+                'email': 'joe@example.com',
+                'password': "wrongpass"
+            }),
+            content_type="application/json")
+        res = json.loads(login_res.data)
+        self.assertEqual(res['Message'], 'Incorrect Email or Password')
+
+    def test_unregistered_user_login(self):
+        none_exist = {"email": "fake@email.com", "password": 'aaabbbcccddd'}
+        login_res = self.client().post(
+            'api/v1/auth/login',
+            content_type='application/json',
+            data=json.dumps(none_exist))
+        result = json.loads(login_res.data)
+        self.assertEqual(result['Message'], "Incorrect Email or Password")
+        self.assertEqual(login_res.status_code, 411)
 
     def test_already_registered(self):
         """Test register a user who is already registered"""
         self.client().post(
-            '/api/v1/register',
+            '/api/v1/auth/register',
             data=json.dumps(self.user_details),
             content_type="application/json")
 
         second_res = self.client().post(
-            '/api/v1/register',
+            '/api/v1/auth/register',
             data=json.dumps(self.user_details),
             content_type="application/json")
         result = json.loads(second_res.data)
-        self.assertEqual(result['message'], 'Email already Exist')
+        self.assertEqual(result['Message'], 'Email already Exist')
