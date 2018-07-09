96226e8497e4b0ded1a6d2998966cf1874a9c04d
blac-siren <zac.blacksiren@gmail.com>
2018-07-09 05:12:50 +0300

    Refactor code

--- Api/app/configuration/config.py
+++ Api/app/configuration/config.py
@@ -12,10 +12,10 @@ class Config:
 class DevelopmentConfig(Config):
     """Configuration for development."""
 
-    DEBUG = True
+    DEBUG = False
     TESTING = False
-    DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:zakaria@localhost/trackerapp')
-
+    DATABASE_URI = os.environ.get(
+        'DATABASE_URL', 'postgresql://postgres:zakaria@localhost/trackerapp')
 
 
 class TestingConfig(Config):
@@ -23,7 +23,8 @@ class TestingConfig(Config):
 
     TESTING = True
     DEBUG = True
-    DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:zakaria@localhost/trackertest')
+    DATABASE_URI = os.environ.get(
+        'DATABASE_URL', 'postgresql://postgres:zakaria@localhost/trackertest')
 
 
 class ProductionConfig(Config):
--- Api/run_migration.py
+++ Api/run_migration.py
@@ -2,7 +2,7 @@ from app.DB import table_db
 from app.DB import defaultAdmin
 
 # runs migration- create table
-table_db.run_migrations('testing')
+table_db.run_migrations('development')
 
 # create default admin
-defaultAdmin.create_admin('testing')
+defaultAdmin.create_admin('development')
