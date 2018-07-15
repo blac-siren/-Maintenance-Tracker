5aa7a9f1fd6bc72ff6bda675fc27b9c8c6926317
blac-siren <zac.husnein@gmail.com>
2018-07-15 14:03:15 +0300

    Update migration file
    '
    ;
    git status

--- Api/run_migration.py
+++ Api/run_migration.py
@@ -2,7 +2,7 @@ from app.DB import table_db
 from app.DB import defaultAdmin
 
 # runs migration- create table
-table_db.run_migrations('development')
+table_db.run_migrations('testing')
 
 # create default admin
-defaultAdmin.create_admin('development')
+defaultAdmin.create_admin('testing')
